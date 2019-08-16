26709d65a1d4e3272d5475aab9a363753db1db37
hussyn12 <hussyn12@outlook.com>
2019-08-16 11:50:10 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -134,7 +134,7 @@ $(document).ready(function(){
   $('body').scrollspy({target: ".navbar", offset: 50});   
 
   // Add smooth scrolling on all links inside the navbar
-  $(".header a, .scrollbtn, .contact, .work, .dark-border").on('click', function(event) {
+  $(".header a, .scrollbtn, .work, .dark-border").on('click', function(event) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
@@ -145,7 +145,7 @@ $(document).ready(function(){
 
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
+      $('html,body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
    
